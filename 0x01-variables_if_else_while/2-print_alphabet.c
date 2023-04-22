b84fdd6aecb589f924da7019b17d111bbca606fe
Bethelomeke <bethelomeke@gmail.com>
2023-04-21 20:24:36 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry print
+ * Description: 'Print alphabets lowercase'
+ * Return: Always 0
+ */
+int main(void)
+{
+	putchar(65);
+	putchar('\n');
+	return (0);
+}
